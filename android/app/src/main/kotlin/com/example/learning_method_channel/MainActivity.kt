e2b98e2b226ccf62a7f9067010493affd1d29888
smnasimahmed <nasimfarsid@gmail.com>
2026-06-16 17:35:22 +0600

    starting on kotlin

--- android/app/src/main/kotlin/com/example/learning_method_channel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/learning_method_channel/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.learning_method_channel
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity(){
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+  String _batteryLevel = 'Unknown battery level';
+
+  Future<void> _getBatteryLevel() async {
+    String battryLevel;
+    try {
+      final result = await platform.invokeMethod('getBatteryLevel');
+      battryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      battryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = battryLevel;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: _getBatteryLevel,
+              child: const Text('Get Battery Level'),
+            ),
+            Text(_batteryLevel),
+          ],
         ),
       ),
     );
